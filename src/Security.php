c08b54b0c2c1732a0053f4960c0ea1c9501b7aa7
Lars Moelleken <lars.moelleken@vdmg-it.com>
2019-11-11 16:23:27 +0100

    use "voku/anti-xss"

--- src/Security.php
+++ src/Security.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 
 namespace GrahamCampbell\SecurityCore;
 
+use voku\helper\AntiXSS;
+
 /**
  * This is the security class.
  *
@@ -24,29 +26,43 @@ namespace GrahamCampbell\SecurityCore;
  * @author Derek Jones <derek.jones@ellislab.com>
  * @author Graham Campbell <graham@alt-three.com>
  */
-class Security
+final class Security
 {
     /**
-     * The evil attributes.
-     *
-     * @var string[]
+     * @var AntiXSS
      */
-    protected $evil;
+    private $antiXss;
 
     /**
      * Create a new security instance.
      *
      * @param string[]|null $evil
+     * @param string        $replacement
+     * @param AntiXSS|null  $antiXss
      *
      * @return void
      */
-    public function __construct(array $evil = null)
+    public function __construct(array $evil = null, string $replacement = '', AntiXSS $antiXss = null)
     {
-        $this->evil = $evil ?: ['(?<!\w)on\w*', 'style', 'xmlns', 'formaction', 'form', 'xlink:href', 'FSCommand', 'seekSegmentTime'];
+        $evilRegex = [];
+        if ($evil && $evil !== []) {
+            foreach ($evil as $regex) {
+                $evilRegex[$regex] = $replacement;
+            }
+        }
+
+        if ($antiXss === null) {
+            $this->antiXss = new AntiXSS();
+        } else {
+            $this->antiXss = $antiXss;
+        }
+
+        $this->antiXss->setReplacement($replacement);
+        $this->antiXss->addNeverAllowedRegex($evilRegex);
     }
 
     /**
-     * XSS clean.
+     * XSS clean
      *
      * @param string|string[] $str
      *
@@ -54,266 +70,6 @@ class Security
      */
     public function clean($str)
     {
-        if (is_array($str)) {
-            foreach ($str as $key => &$value) {
-                $str[$key] = $this->clean($value);
-            }
-
-            return $str;
-        }
-
-        $i = 0;
-        $hash = bin2hex(random_bytes(20));
-
-        do {
-            $i++;
-            $processed = $this->process($str, $hash);
-        } while ($i < 3 && $processed !== $str);
-
-        return $processed;
-    }
-
-    /**
-     * Process a string for cleaning.
-     *
-     * @param string $str
-     * @param string $hash
-     *
-     * @return string
-     */
-    protected function process(string $str, string $hash)
-    {
-        $str = self::removeInvisibleCharacters($str);
-
-        if (stripos($str, '%') !== false) {
-            do {
-                $original = $str;
-                $str = preg_replace_callback(
-                    '#%(?:\s*[0-9a-f]){2,}#i',
-                    [$this, 'urlDecodeSpaces'],
-                    rawurldecode($str)
-                );
-            } while ($original !== $str);
-
-            unset($original);
-        }
-
-        $str = preg_replace_callback(
-            "/[^a-z0-9>]+[a-z0-9]+=([\'\"]).*?\\1/si",
-            [$this, 'convertAttribute'],
-            $str
-        );
-
-        $str = preg_replace_callback(
-            '/<\w+.*?(?=>|<|$)/si',
-            Utils::decodeEntityCallback($hash),
-            $str
-        );
-
-        $str = self::removeInvisibleCharacters($str);
-
-        $str = str_replace("\t", ' ', $str);
-
-        $str = Utils::doNeverAllowed($str);
-
-        $str = str_replace(['<?', '?'.'>'], ['&lt;?', '?&gt;'], $str);
-
-        $words = [
-            'javascript', 'expression', 'vbscript', 'jscript', 'wscript',
-            'vbs', 'script', 'base64', 'applet', 'alert', 'document',
-            'write', 'cookie', 'window', 'confirm', 'prompt', 'eval',
-        ];
-
-        foreach ($words as $word) {
-            $word = implode('\s*', str_split($word)).'\s*';
-            $str = preg_replace_callback(
-                '#('.substr($word, 0, -3).')(\W)#is',
-                [$this, 'compactExplodedWords'],
-                $str
-            );
-        }
-
-        do {
-            $original = $str;
-
-            if (preg_match('/<a/i', $str)) {
-                $str = preg_replace_callback(
-                    '#<a[^a-z0-9>]+([^>]*?)(?:>|$)#si',
-                    [$this, 'jsLinkRemoval'],
-                    $str
-                );
-            }
-
-            if (preg_match('/<img/i', $str)) {
-                $str = preg_replace_callback(
-                    '#<a(?:rea)?[^a-z0-9>]+([^>]*?)(?:>|$)#si',
-                    [$this, 'jsImgRemoval'],
-                    $str
-                );
-            }
-
-            if (preg_match('/script|xss/i', $str)) {
-                $str = preg_replace('#</*(?:script|xss).*?>#si', '[removed]', $str);
-            }
-        } while ($original !== $str);
-
-        unset($original);
-
-        $pattern = '#'
-            .'<((?<slash>/*\s*)((?<tagName>[a-z0-9]+)(?=[^a-z0-9]|$)|.+)'
-            .'[^\s\042\047a-z0-9>/=]*'
-            .'(?<attributes>(?:[\s\042\047/=]*'
-            .'[^\s\042\047>/=]+'
-                .'(?:\s*='
-                    .'(?:[^\s\042\047=><`]+|\s*\042[^\042]*\042|\s*\047[^\047]*\047|\s*(?U:[^\s\042\047=><`]*))'
-                .')?'
-            .')*)'
-            .'[^>]*)(?<closeTag>\>)?#isS';
-
-        do {
-            $original = $str;
-            $str = preg_replace_callback($pattern, Utils::sanitizeHtmlCallback($this->evil), $str);
-        } while ($original !== $str);
-
-        unset($original);
-
-        $str = preg_replace(
-            '#(alert|prompt|confirm|cmd|passthru|eval|exec|expression|system|fopen|fsockopen|file|file_get_contents|readfile|unlink)(\s*)\((.*?)\)#si',
-            '\\1\\2&#40;\\3&#41;',
-            $str
-        );
-
-        $str = preg_replace(
-            '#(alert|prompt|confirm|cmd|passthru|eval|exec|expression|system|fopen|fsockopen|file|file_get_contents|readfile|unlink)(\s*)`(.*?)`#si',
-            '\\1\\2&#96;\\3&#96;',
-            $str
-        );
-
-        return Utils::doNeverAllowed($str);
-    }
-
-    /**
-     * Removes invisible characters.
-     *
-     * @param string $str
-     * @param bool   $urlEncoded
-     *
-     * @return string
-     */
-    private static function removeInvisibleCharacters(string $str, bool $urlEncoded = true)
-    {
-        $nonDisplayables = [];
-
-        if ($urlEncoded) {
-            $nonDisplayables[] = '/%0[0-8bcef]/';
-            $nonDisplayables[] = '/%1[0-9a-f]/';
-        }
-
-        $nonDisplayables[] = '/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S';
-
-        do {
-            $str = preg_replace($nonDisplayables, '', $str, -1, $count);
-        } while ($count);
-
-        return $str;
-    }
-
-    /**
-     * URL decode taking spaces into account.
-     *
-     * @param string[] $matches
-     *
-     * @return string
-     */
-    private static function urlDecodeSpaces(array $matches)
-    {
-        $input = $matches[0];
-        $nospaces = preg_replace('#\s+#', '', $input);
-
-        return $nospaces === $input ? $input : rawurldecode($nospaces);
-    }
-
-    /**
-     * Compact exploded words.
-     *
-     * @param string[] $matches
-     *
-     * @return string
-     */
-    private static function compactExplodedWords(array $matches)
-    {
-        return preg_replace('/\s+/s', '', $matches[1]).$matches[2];
-    }
-
-    /**
-     * JS link removal.
-     *
-     * @param string[] $match
-     *
-     * @return string
-     */
-    private static function jsLinkRemoval(array $match)
-    {
-        return str_replace(
-            $match[1],
-            preg_replace(
-                '#href=.*?(?:(?:alert|prompt|confirm)(?:\(|&\#40;|`|&\#96;)|javascript:|livescript:|mocha:|charset=|window\.|\(?document\)?\.|\.cookie|<script|<xss|d\s*a\s*t\s*a\s*:)#si',
-                '',
-                self::filterAttributes($match[1])
-            ),
-            $match[0]
-        );
-    }
-
-    /**
-     * JS image removal.
-     *
-     * @param string[] $match
-     *
-     * @return string
-     */
-    private static function jsImgRemoval(array $match)
-    {
-        return str_replace(
-            $match[1],
-            preg_replace(
-                '#src=.*?(?:(?:alert|prompt|confirm|eval)(?:\(|&\#40;|`|&\#96;)|javascript:|livescript:|mocha:|charset=|window\.|\(?document\)?\.|\.cookie|<script|<xss|base64\s*,)#si',
-                '',
-                self::filterAttributes($match[1])
-            ),
-            $match[0]
-        );
-    }
-
-    /**
-     * Attribute conversion.
-     *
-     * @param string[] $match
-     *
-     * @return string
-     */
-    private static function convertAttribute(array $match)
-    {
-        return str_replace(['>', '<', '\\'], ['&gt;', '&lt;', '\\\\'], $match[0]);
-    }
-
-    /**
-     * Attribute filtering.
-     *
-     * @param string $str
-     *
-     * @return string
-     */
-    private static function filterAttributes(string $str)
-    {
-        $out = '';
-
-        if (preg_match_all('#\s*[a-z\-]+\s*=\s*(\042|\047)([^\\1]*?)\\1#is', $str, $matches)) {
-            foreach ($matches[0] as $match) {
-                $out .= preg_replace('#/\*.*?\*/#s', '', $match);
-            }
-        }
-
-        return $out;
+        return $this->antiXss->xss_clean($str);
     }
 }
--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -16,6 +16,7 @@ namespace GrahamCampbell\Tests\SecurityCore;
 
 use GrahamCampbell\SecurityCore\Security;
 use PHPUnit\Framework\TestCase;
+use voku\helper\AntiXSS;
 
 /**
  * This is the security test class.
@@ -29,11 +30,11 @@ class SecurityTest extends TestCase
         $cases = [
             [
                 'Hello, try to <script>alert(\'Hack\');</script> this site',
-                'Hello, try to [removed]alert&#40;\'Hack\'&#41;;[removed] this site',
+                'Hello, try to  this site',
             ],
             [
                 '<a href="&#38&#35&#49&#48&#54&#38&#35&#57&#55&#38&#35&#49&#49&#56&#38&#35&#57&#55&#38&#35&#49&#49&#53&#38&#35&#57&#57&#38&#35&#49&#49&#52&#38&#35&#49&#48&#53&#38&#35&#49&#49&#50&#38&#35&#49&#49&#54&#38&#35&#53&#56&#38&#35&#57&#57&#38&#35&#49&#49&#49&#38&#35&#49&#49&#48&#38&#35&#49&#48&#50&#38&#35&#49&#48&#53&#38&#35&#49&#49&#52&#38&#35&#49&#48&#57&#38&#35&#52&#48&#38&#35&#52&#57&#38&#35&#52&#49">Clickhere</a>',
-                '<a>Clickhere</a>',
+                '<a href="">Clickhere</a>',
             ],
             [
                 '&foo should not include a semicolon',
@@ -45,143 +46,143 @@ class SecurityTest extends TestCase
             ],
             [
                 '<div style="color:rgb(\'\'&#0;x:expression(alert(1))"></div>',
-                '<div xss=removed></div>',
+                '<div ></div>',
             ],
             [
                 '<img/src=%00 id=confirm(1) onerror=eval(id)',
-                '&lt;img/src= id=confirm&#40;1&#41; onerror=eval&#40;id&#41;',
+                '&lt;img/',
             ],
             [
                 '<div id=confirm(1) onmouseover=eval(id)>X</div>',
-                '<div id=confirm&#40;1&#41; xss=removed>X</div>',
+                '<div id=confirm&#40;1&#41; >X</div>',
             ],
             [
                 '<span/onmouseover=confirm(1)>X</span>',
-                '<span xss=removed>X</span>',
+                '<span/>X</span>',
             ],
             [
                 '<svg/contentScriptType=text/vbs><script>Execute(MsgBox(chr(88)&chr(83)&chr(83)))',
-                '&lt;svg/contentScriptType=text/vbs&gt;[removed]Execute(MsgBox(chr(88)&chr(83)&chr(83)))',
+                '&lt;svg/contentScriptType=text/vbs&gt;',
             ],
             [
                 '<iframe/src="javascript:a=[alert&lpar;1&rpar;,confirm&#40;2&#41;,prompt%283%29];eval(a[0]);">',
-                '&lt;iframe/src="[removed]a=[alert&#40;1&#41;,confirm&#40;2&#41;,prompt&#40;3&#41;];eval&#40;a[0]&#41;;"&gt;',
+                '&lt;iframe/src="a=[alert&#40;1&#41;,confirm&#40;2&#41;,prompt&#40;3&#41;];eval&#40;a[0]&#41;;"&gt;',
             ],
             [
                 '<div/style=content:url(data:image/svg+xml);visibility:visible onmouseover=alert(1)>x</div>',
-                '<div xss=removed xss=removed>x</div>',
+                '<div/ >x</div>',
             ],
             [
                 '<script>Object.defineProperties(window,{w:{value:{f:function(){return 1}}}});confirm(w.f())</script>',
-                '[removed]Object.defineProperties(window,{w:{value:{f:function(){return 1}}}});confirm&#40;w.f(&#41;)[removed]',
+                '',
             ],
             [
                 '<keygen/onfocus=prompt(1);>',
-                '&lt;keygen/onfocus=prompt&#40;1&#41;;&gt;',
+                '&lt;keygen/&gt;',
             ],
             [
                 '<img/src=`%00` id=confirm(1) onerror=eval(id)',
-                '&lt;img/src=`` id=confirm&#40;1&#41; onerror=eval&#40;id&#41;',
+                '&lt;img/',
             ],
             [
                 '<img/src=`%00` onerror=this.onerror=confirm(1)',
-                '&lt;img/src=`` onerror=this.onerror=confirm&#40;1&#41;',
+                '&lt;img/',
             ],
             [
                 '<iframe/src="data:text/html,<iframe%09onload=confirm(1);>">',
-                '&lt;iframe/src="data:text/html,&lt;iframe onload=confirm&#40;1&#41;;&gt;"&gt;',
+                '&lt;iframe/src="data:text/html,&lt;iframe	>">',
             ],
             [
                 '<math><a/xlink:href=javascript:prompt(1)>X',
-                '&lt;math&gt;<a>X',
+                '&lt;math&gt;&lt;a/>X',
             ],
             [
                 '<input/type="image"/value=""`<span/onmouseover=\'confirm(1)\'>X`</span>',
-                '&lt;input/type="image"/value=""`<span xss=removed>',
+                '&lt;input/type="image"/value=""`&lt;span/>X`</span>',
             ],
             [
                 '<form/action=javascript&#x0003A;eval(setTimeout(confirm(1)))><input/type=submit>',
-                '&lt;form/action=[removed]eval&#40;setTimeout(confirm(1&#41;))&gt;&lt;input/type=submit&gt;',
+                '&lt;form/action=eval&#40;setTimeout(confirm(1&#41;))&gt;&lt;input/type=submit>',
             ],
             [
                 '<body/onload=this.onload=document.body.innerHTML=alert&lpar;1&rpar;>',
-                '&lt;body/onload=this.onload=document.body[removed]=alert&#40;1&#41;&gt;',
+                '&lt;body/&gt;',
             ],
             [
                 '<iframe/onload=\'javascript&#58;void&#40;1&#41;&quest;void&#40;1&#41;&#58;confirm&#40;1&#41;\'>',
-                '&lt;iframe/onload=\'[removed]void(1)?void(1):confirm&#40;1&#41;\'&gt;',
+                '&lt;iframe/&gt;',
             ],
             [
                 '<object/type="text/x-scriptlet"/data="data:X,&#60script&#62setInterval&lpar;\'prompt(1)\',10&rpar;&#60/script&#62"></object>',
-                '&lt;object/type="text/x-scriptlet"/data="data:X,[removed]setInterval(\'prompt&#40;1&#41;\',10)[removed]"&gt;&lt;/object&gt;',
+                '&lt;object/type="text/x-scriptlet"/data="data:X,"&gt;&lt;/object>',
             ],
             [
                 '<i<f<r<a<m<e><iframe/onload=confirm(1);></i>f>r>a>m>e>',
-                '<i>&lt;iframe/onload=confirm&#40;1&#41;;&gt;</i>f>r>a>m>e>',
+                '<i&lt;f&lt;r&lt;a&lt;me>&lt;iframe/&gt;&lt;/i>f>r>a>m>e>',
             ],
             [
                 'http://www.<script abc>setTimeout(\'confirm(1)\',1)</script .com>',
-                'http://www.[removed]setTimeout(\'confirm&#40;1&#41;\',1)[removed]',
+                'http://www.setTimeout(\'confirm&#40;1&#41;\',1)',
             ],
             [
                 '<style/onload    =    !-alert&#x28;1&#x29;>',
-                '&lt;style/onload    =    !-alert&#40;1&#41;&gt;',
+                '&lt;style/&gt;',
             ],
             [
                 '<svg id=a /><script language=vbs for=a event=onload>alert 1</script>',
-                '&lt;svg id=a /&gt;[removed]alert 1[removed]',
+                '&lt;svg id=a /&gt;alert 1',
             ],
             [
                 '<object/data="data&colon;X&comma;&lt;script&gt;alert&#40;1&#41;%3c&sol;script%3e">',
-                '&lt;object/data="data:X,[removed]alert&#40;1&#41;[removed]"&gt;',
+                '&lt;object/data="data:X,"&gt;',
             ],
             [
                 '<form/action=javascript&#x3A;void(1)&quest;void(1)&colon;alert(1)><input/type=\'submit\'>',
-                '&lt;form/action=[removed]void(1)?void(1):alert&#40;1&#41;&gt;&lt;input/type=\'submit\'&gt;',
+                '&lt;form/action=void(1)?void(1):alert&#40;1&#41;&gt;&lt;input/type=\'submit\'>',
             ],
             [
                 '<iframe/srcdoc=\'&lt;iframe&sol;onload&equals;confirm(&sol;&iexcl;&hearts;&xcup;&sol;)&gt;\'>',
-                '&lt;iframe/srcdoc=\'&lt;iframe/onload=confirm&#40;/¡♥⋃/&#41;&gt;\'&gt;',
+                '&lt;iframe/srcdoc=\'&lt;iframe/>\'>',
             ],
             [
                 '<meta/http-equiv="refresh"/content="0;url=javascript&Tab;:&Tab;void(alert(0))?0:0,0,prompt(0)">',
-                '&lt;meta/http-equiv="refresh"/content="0;url=[removed] void(alert&#40;0&#41;)?0:0,0,prompt&#40;0&#41;"&gt;',
+                '&lt;meta/http-equiv="refresh"/content="0;url=	void(alert&#40;0&#41;)?0:0,0,prompt&#40;0&#41;"&gt;',
             ],
             [
                 '<script src="h&Tab;t&Tab;t&Tab;p&Tab;s&colon;/&Tab;/&Tab;http://dl.dropbox.com/u/13018058/js.js"></script>',
-                '[removed][removed]',
+                '',
             ],
             [
                 '<style/onload=\'javascript&colon;void(0)?void(0)&colon;confirm(1)\'>',
-                '&lt;style/onload=\'[removed]void(0)?void(0):confirm&#40;1&#41;\'&gt;',
+                '&lt;style/&gt;',
             ],
             [
                 '<svg><style>&#x7B;-o-link-source&#x3A;\'<style/onload=confirm(1)>\'&#x7D;',
-                '&lt;svg&gt;&lt;style&gt;&#x7B;-o-link-source&#x3A;\'&lt;style/onload=confirm&#40;1&#41;&gt;\'&#x7D;',
+                '&lt;svg&gt;&lt;style>{-o-link-source:\'&lt;style/&gt;\'}',
             ],
             [
                 '<math><solve i.e., x=2+2*2-2/2=? href="data:text/html,<script>prompt(1)</script>">X',
-                '&lt;math&gt;<solve x=2+2*2-2/2 href="data:text/html,[removed]prompt&#40;1&#41;[removed]">X',
+                '&lt;math&gt;&lt;solve i.e., x=2+2*2-2/2=? href="data:text/html,">X',
             ],
             [
                 '<iframe/src="j&Tab;AVASCRIP&NewLine;t:\u0061ler\u0074&#x28;1&#x29;">',
-                '&lt;iframe/src="[removed]\\\u0061ler\\\u0074(1)"&gt;',
+                '&lt;iframe/src="alert&#40;1&#41;"&gt;',
             ],
             [
                 '<iframe/src="javascript:void(alert(1))?alert(1):confirm(1),prompt(1)">',
-                '&lt;iframe/src="[removed]void(alert&#40;1&#41;)?alert&#40;1&#41;:confirm&#40;1&#41;,prompt&#40;1&#41;"&gt;',
+                '&lt;iframe/src="void(alert&#40;1&#41;)?alert&#40;1&#41;:confirm&#40;1&#41;,prompt&#40;1&#41;"&gt;',
             ],
             [
                 '<embed/src=javascript&colon;\u0061&#x6C;&#101%72t&#x28;1&#x29;>',
-                '&lt;embed/src=[removed]\u0061lert(1)&gt;',
+                '&lt;embed/src=alert&#40;1&#41;&gt;',
             ],
             [
                 '<img/src=\'http://i.imgur.com/P8mL8.jpg \' onmouseover={confirm(1)}f()>',
-                '<img src=\'http://i.imgur.com/P8mL8.jpg \' xss=removed>',
+                '<img/src=\'http://i.imgur.com/P8mL8.jpg \'>',
             ],
             [
                 '<style/&Tab;/onload=;&Tab;this&Tab;.&Tab;onload=confirm(1)>',
-                '&lt;style/ /onload=; this . onload=confirm&#40;1&#41;&gt;',
+                '&lt;style/	/	this	.	&gt;',
             ],
             [
                 '<embed/src=//goo.gl/nlX0P>',
@@ -189,31 +190,31 @@ class SecurityTest extends TestCase
             ],
             [
                 '<form><button formaction=javascript:alert(1)>CLICKME',
-                '&lt;form&gt;&lt;button formaction=[removed]alert&#40;1&#41;&gt;CLICKME',
+                '&lt;form&gt;&lt;button >CLICKME',
             ],
             [
                 '<script>x=\'con\';s=\'firm\';S=\'(1)\';setTimeout(x+s+S,0);</script>',
-                '[removed]x=\'con\';s=\'firm\';S=\'(1)\';setTimeout(x+s+S,0);[removed]',
+                '',
             ],
             [
                 '<img/id="confirm&lpar;1&#x29;"/alt="/"src="/"onerror=eval(id&#x29;>',
-                '<img id="confirm&#40;1&#41;" alt="/" src="/" xss=removed>',
+                '<img/id="confirm&#40;1&#41;"alt="/"src="/">',
             ],
             [
                 '<iframe/src="data&colon;text&sol;html,<s&Tab;cr&Tab;ip&Tab;t>confirm(1)</script>">',
-                '&lt;iframe/src="data:text/html,[removed]confirm&#40;1&#41;[removed]"&gt;',
+                '&lt;iframe/src="data:text/html,"&gt;',
             ],
             [
                 '<foo fscommand=case-insensitive><foo seekSegmentTime=whatever>',
-                '<foo xss=removed><foo xss=removed>',
+                '<foo ><foo >',
             ],
             [
                 '<foo onAttribute="bar">',
-                '<foo xss=removed>',
+                '<foo >',
             ],
             [
                 '<foo onAttributeWithSpaces = bar>',
-                '<foo xss=removed>',
+                '<foo >',
 
             ],
             [
@@ -222,23 +223,23 @@ class SecurityTest extends TestCase
             ],
             [
                 "\n><!-\n<b\n<c d=\"'e><iframe onload=alert(1) src=x>\n<a HREF=\"\">\n",
-                "\n>&lt;!-\n<b d=\"'e><iframe onload=alert&#40;1&#41; src=x>\n<a HREF=\">\n",
+                "\n>&lt;!-\n&lt;b\n&lt;c d=\"'e&gt;&lt;iframe  src=x&gt;\n&lt;a \"\"&gt;\n",
             ],
             [
                 '<meta charset="x-imap4-modified-utf7">&<script&S1&TS&1>alert&A7&(1)&R&UA;&&<&A9&11/script&X&>',
-                '&lt;meta charset="x-imap4-modified-utf7"&gt;&[removed]alert&A7&(1)&R&UA;&&&lt;&A9&11/script&X&>',
+                '&lt;meta charset="x-imap4-modified-utf7"&gt;&alert&A7&(1)&R&UA;&&&lt;&A9&11/script&X&&gt;',
             ],
             [
                 '<!--\\x3E<img src=xxx:x onerror=javascript:alert(1)> -->',
-                '&lt;!--\\x3E<img src=xxx:x xss=removed> --&gt;',
+                '&lt;!--\x3E<img > -->',
             ],
             [
                 '--><!-- --\x3E> <img src=xxx:x onerror=javascript:alert(1)> -->',
-                '--&gt;&lt;!-- --\x3E> <img src=xxx:x xss=removed> --&gt;',
+                '-->&lt;!-- --\x3E> <img > -->',
             ],
             [
                 '<svg/onload=alert(1)',
-                '&lt;svg/onload=alert&#40;1&#41;',
+                '&lt;svg/',
             ],
         ];
 
@@ -259,11 +260,23 @@ class SecurityTest extends TestCase
 
     public function testCleanArray()
     {
-        $security = $this->getSecurity();
+        $security = new Security(['test.*'], '[removed]');
 
-        $return = $security->clean(['test', '123', ['abc']]);
+        $return = $security->clean(['<script>', 'testFoo', '123', ['abc']]);
 
-        $this->assertSame(['test', '123', ['abc']], $return);
+        $this->assertSame(['[removed]', '[removed]', '123', ['abc']], $return);
+    }
+
+    public function testCleanWithCustomAntiXss()
+    {
+        $antiXss = new AntiXSS();
+        $antiXss->removeEvilAttributes(array('style')); // allow style-attributes
+
+        $security = new Security(['test.*'], '[removed]', $antiXss);
+
+        $return = $security->clean(['<script>', 'testFoo', '123', ['<abc style="border: 0;">']]);
+
+        $this->assertSame(['[removed]', '[removed]', '123', ['<abc style="border: 0;">']], $return);
     }
 
     protected function getSecurity()
